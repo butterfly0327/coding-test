e5a56fe146379addf663501d2b02a39040e739d3
gyu_dong <pgda397@gmail.com>
2026-07-25 11:50:37 +0900

    [level 1] Title: 덧칠하기, Time: 1.05 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        
+        int answer = 0;
+        int end = 0;
+        
+        for(int cur : section){
+            if(cur > end){
+                answer++;
+                end = cur + m - 1;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
